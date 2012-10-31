a5aa60db98d12b0ada2c28ed7014090a5f02c598
Javier Perez-Griffo <javier@besol.es>
2012-10-31 18:19:30 +0100

    Inclusion of Windows Recepies

---
+++ README.md
@@ -0,0 +1,9 @@
+Description
+===========
+
+Install Adobe Reader with the default recipe and manage package with LWRP
+
+Requirements
+============
+
+Work on Windows
---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Javier Perez-Griffo"
+maintainer_email "javier@tapp.in"
+license          "Apache 2.0"
+description      "Install Adobe Reader in Windows"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.3"
+
+depends "chocolatey"
+supports "windows"
